517b988f21f1303d971c6991224e7535477f23db
PomPomegranates <persephoneapplewhite@gmail.com>
2024-12-18 10:46:15 +0000

    Visual Studio Files Added

---
+++ NC_MVC/Program.cs
@@ -0,0 +1,15 @@
+namespace NC_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
